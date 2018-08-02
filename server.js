88b1ad09452e3dd98dca0fd5b91eabcd1d9746e3
Karolina Piróg <karolina.swieca@wp.pl>
2018-08-02 11:41:26 +0200

    add app.listen(3000) in server.js

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 var fs = require('fs');
 var app = express();
-var stringfyFile;
+var stringfyFile = req.params.note ;
 
 app.use(bodyParser.json());
 
 app.get('/getNote', function (req, res) {
-    res.send(
+    res.send()
 });
+
+fs.readFile('./test.json', 'utf8', function(err, data) {
+    if (err) throw err;
+    stringifyFile = data
+    res.send(data);
+});
+
+fs.writeFile('./test.json', stringifyFile, function(err) {
+    if (err) throw err;
+    console.log('file updated');
+});
+
+app.post();
+
+app.listen(3000);
